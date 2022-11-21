1194b415919498f2f6c746686c5dd7ce4757ec02
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-11-22 03:13:13 +0530

    Coded Logic for Fetching Name By ID

--- src/main/java/com/hari/demo/dao/PersonDao.java
+++ src/main/java/com/hari/demo/dao/PersonDao.java
@@ -3,6 +3,7 @@ package com.hari.demo.dao;
 import com.hari.demo.model.Person;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PersonDao {
@@ -15,4 +16,10 @@ public interface PersonDao {
     }
 
     List<Person> selectAllPeople();
+
+    int deletePersonById(UUID id);
+
+    int updatePersonById(UUID id, Person person);
+
+    Optional<Person> selectPersonById(UUID id);
 }
