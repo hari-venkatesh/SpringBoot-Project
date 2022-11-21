36078b700d8978e33a83dc0a1d4888fe549d6ff1
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-11-22 02:37:03 +0530

    First Api Request went in Successfully

--- src/main/java/com/hari/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/hari/demo/dao/FakePersonDataAccessService.java
@@ -1,21 +1,20 @@
 package com.hari.demo.dao;
 
 import com.hari.demo.model.Person;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonDao{
     private static List<Person> DB = new ArrayList<>();
 
     @Override
     public int insertPerson(UUID id, Person person) {
-        return 0;
+        DB.add(new Person(id, person.getName()));
+        return 1;
     }
 
-    @Override
-    public int addPerson(Person person) {
-        return PersonDao.super.addPerson(person);
-    }
 }
